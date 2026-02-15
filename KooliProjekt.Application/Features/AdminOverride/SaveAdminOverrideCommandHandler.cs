d987d4151c5ef26a960306a3adfa32a93ba72fca
siim803 <siimuutma7@gmail.com>
2026-02-15 22:18:35 +0200

    AdminOverride class features done

---
+++ KooliProjekt.Application/Features/AdminOverride/SaveAdminOverrideCommandHandler.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using KooliProjekt.Application.Data;
+using KooliProjekt.Application.Infrastructure.Paging;
+using KooliProjekt.Application.Infrastructure.Results;
+using MediatR;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace KooliProjekt.Application.Features.AdminOverride
+{
+	public class SaveAdminOverrideCommandHandler : IRequestHandler<SaveAdminOverrideCommand, OperationResult>
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public SaveAdminOverrideCommandHandler(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<OperationResult> Handle(SaveAdminOverrideCommand request, CancellationToken cancellationToken)
+		{
+			var result = new OperationResult();
+
+			var list = new AdminOverride();
+			if (request.Id == 0)
+			{
+				await _dbContext.AdminOverride.AddAsync(list);
+			}
+			else
+			{
+				list = await _dbContext.AdminOverride.FindAsync(request.Id);
+			}
+
+			list.Title = request.Title;
+
+			await _dbContext.SaveChangesAsync();
+
+			return result;
+		}
+	}
+}
